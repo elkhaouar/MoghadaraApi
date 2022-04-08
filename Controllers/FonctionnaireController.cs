382e3cb89874a80bce8c7dbf02fe45b7c83ff448
redONE <elkhaouar@gmail.com>
2022-04-08 00:06:22 +0000

    add repo and services

---
+++ Controllers/FonctionnaireController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoghadaraApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FonctionnaireController : ControllerBase
+    {
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MoghadaraApi.Models;
 
 namespace MoghadaraApi
 {
@@ -25,7 +27,11 @@ namespace MoghadaraApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
+            services.AddDbContext<MoghadaraateContext>(o =>
+            {
+                o.UseSqlServer("Server=localhost;Database=Moghadaraate;Trusted_Connection=True;");
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
