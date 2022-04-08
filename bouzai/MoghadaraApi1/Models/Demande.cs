c841f2d1a99abec0a266b2bd770b764ad20637e9
redONE <elkhaouar@gmail.com>
2022-04-08 16:59:00 +0000

    add other library

---
+++ bouzai/MoghadaraApi1/Models/Demande.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MoghadaraApi.Models
+{
+    public partial class Demande
+    {
+        public int IdDemd { get; set; }
+        public string IdQuitterTerritoir { get; set; }
+        public string NumDemande { get; set; }
+        public string UserSaisie { get; set; }
+        public string DateSaisie { get; set; }
+        public string Valide { get; set; }
+        public string Cause { get; set; }
+        public string DateDebut { get; set; }
+        public string Dure { get; set; }
+        public string DateValideTab { get; set; }
+        public string TelPay { get; set; }
+        public string AdressPay { get; set; }
+        public string InfoSuppTab { get; set; }
+        public string DateAutorisation { get; set; }
+        public double? FlagSupp { get; set; }
+        public int? IdFonct { get; set; }
+        public int? IdPay { get; set; }
+        public double? IdEtab { get; set; }
+
+        public virtual Etablissement IdEtabNavigation { get; set; }
+        public virtual Fonctionnaire IdFonctNavigation { get; set; }
+        public virtual Pay IdPayNavigation { get; set; }
+    }
+}
---
+++ bouzai/MoghadaraApi1/Models/SourceExcel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MoghadaraApi.Models
+{
+    public partial class SourceExcel
+    {
+        public string IdQuitterTerritoire { get; set; }
+        public string NumDemande { get; set; }
+        public double? Matricule { get; set; }
+        public double? NumSomme { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Cin { get; set; }
+        public string Emails { get; set; }
+        public string Tel { get; set; }
+        public string Dure { get; set; }
+        public string Cause { get; set; }
+        public string Pays { get; set; }
+        public string DateDebut { get; set; }
+        public string TelPays { get; set; }
+        public string AdressPays { get; set; }
+        public string UserSaisie { get; set; }
+        public string DateSaisie { get; set; }
+        public string Valide { get; set; }
+        public string CodeOrg { get; set; }
+        public string LibelleEtablissement { get; set; }
+        public string DateAutorisation { get; set; }
+        public double? FlagSupp { get; set; }
+        public string Datvalidetab { get; set; }
+        public string Infosuppetab { get; set; }
+    }
+}
